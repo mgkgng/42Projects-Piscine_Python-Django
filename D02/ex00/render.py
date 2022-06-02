223b9df54eb93fa60a32be136cf280715297ad6c
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-02 21:29:57 +0200

    D02: ex00 done except error gestion

--- D02/ex00/render.py
+++ D02/ex00/render.py
@@ -1,15 +1,45 @@
+from inspect import getmembers
 import sys, os, re
+import settings
 
 def checkError(argv):
-	if len(argv) != 3:
+	if len(argv) != 2:
 		exit(0)
 	if sys.argv[1][-9:] != ".template" or len(argv[1]) < 10:
+		print("coucou")
 		exit(0)
 
+def getValues():
+	l = getmembers(settings)
+	for i in range(8):
+		l.pop(0)
+	res = dict()
+	for elem in l:
+		res[elem[0]] = elem[1]
+	return res
+
+def getVariables(line):
+	return re.findall('{.*?}', line)
+
+def getReplace(str, values):
+	if str not in values.keys():
+		print(f"Error: variable '{str}' not found in settings")
+		exit(1)
+	return values[str]
+
 def	render(file):
-# remplacer {variable_name} en valeur ecrit en myCV.template
-	pass
+	src = open(file, 'r').readlines()
+	dest = open(file[:-9] + ".html", 'w')
+	values = getValues()
+	for line in src:
+
+		toRender = getVariables(line)
+		if len(toRender) != 0:
+			for elem in toRender:
+				line = line.replace(elem, str(getReplace(elem.lstrip('{').rstrip('}'), values)))
+		dest.write(line)
+	dest.close()
 
 if __name__ == "__main__":
-	checkError(sys.argv)
+	#checkError(sys.argv)
 	render(sys.argv[1])
--- D02/ex00/settings.py
+++ D02/ex00/settings.py
@@ -1,1 +1,6 @@
-name = "duoquadragintian"
+page_title = "My CV PAGE"
+name = "Minguk"
+surname = "Kang"
+cv_title = "Welcome to my cv page"
+age = 26
+profession = "programmer"
