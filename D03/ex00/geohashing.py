2ce926f4854cb9113e576bd288f6e7e0971214e7
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-03 13:25:37 +0200

    D03: started. ex00 tried

--- D03/ex00/geohashing.py
+++ D03/ex00/geohashing.py
@@ -1,2 +1,7 @@
 import antigravity, sys
 
+if __name__=="__main__":
+	if len(sys.argv) != 4:
+		print("Error: Wrong number of arguments.")
+		exit(1)
+	antigravity.geohash(sys.argv[1], sys.argv[2], sys.argv[3])
