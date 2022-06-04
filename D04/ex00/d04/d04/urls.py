a608e9d583182379d49e187dcc44c0c8c916383e
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-04 14:01:57 +0200

    D04: ex00 done.

--- D04/ex00/d04/d04/urls.py
+++ D04/ex00/d04/d04/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ex00/', views.ex00)
+    path('ex00/', include('ex00.urls'))
 ]
--- D04/ex00/d04/ex00/views.py
+++ D04/ex00/d04/ex00/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
 
-def ex00(request):
-    return HttpResponse('Hello, World!')
+
+def index(request):
+    return render(request, "ex00/index.html")
+    #return HttpResponse('Hello, World!')
