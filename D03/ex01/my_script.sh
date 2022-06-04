954e7ddf129f2ef69176eec889d5baa5bf927f1d
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-04 13:32:32 +0200

    D04: ex00 started

--- D03/ex01/my_script.sh
+++ D03/ex01/my_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+pip3 -V
 pip3 install git+https://github.com/jaraco/path.git -t ./local_lib --log installation.log --upgrade
 python3 my_program.py
---
+++ D04/ex00/d04/ex00/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render, HttpResponse
+
+def ex00(request):
+    return HttpResponse('Hello, World!')
