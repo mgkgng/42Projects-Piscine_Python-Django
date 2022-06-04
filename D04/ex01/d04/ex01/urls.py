c844f13891f1291236c4f15b0969499c1991ab48
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-04 16:40:25 +0200

    D04: ex01 done

--- D04/ex01/d04/ex01/urls.py
+++ D04/ex01/d04/ex01/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+	path('django', views.django, name='django'),
+	path('display', views.display, name='display'),	
+	path('templates', views.templates, name='templates')
 ]
