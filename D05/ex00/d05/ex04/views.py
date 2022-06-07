846a13bea850a4fbe72fa70ef4113a9a2b1fa4fb
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-07 16:12:47 +0200

    D05: ex04 in progress

--- D05/ex00/d05/ex04/views.py
+++ D05/ex00/d05/ex04/views.py
@@ -1,15 +1,17 @@
-import psycopg2, requests
-from django.shortcuts import render, HttpResponse, redirect
-from .models import Movies
+from django.shortcuts import render, HttpResponse, redirect, HttpResponseRedirect
+import psycopg2
 from .forms import MovieForm
+
+def index(request):
+	return HttpResponse("Hello world!")
 
 def init(request):
 	conn = psycopg2.connect(database="djangotraining", host="localhost", user="djangouser", password="secret")
 	
 	with conn.cursor() as curs:
 		try:
-			curs.execute("""CREATE TABLE ex02names(
-				title varchar(64) NOT NULL UNIQUE
+			curs.execute("""CREATE TABLE IF NOT EXISTS ex04_movies(
+				title varchar(64) NOT NULL UNIQUE,
 				episode_nb serial PRIMARY KEY,
 				opening_crawl text,
 				director varchar(32) NOT NULL,
@@ -26,14 +28,15 @@ def populate(request):
 	conn = psycopg2.connect(database="djangotraining", host="localhost", user="djangouser", password="secret")
 	with conn.cursor() as curs:
 		try:
-			curs.execute("""INSERT INTO djangotraining(title, director, producer, release_date) VALUES
-			("The Phantom Menace", "George Lucas", "Rick McCallum", "1999-05-19"),
-			("Attack of the Clones", "George Lucas", "Rick McCallum", "2002-05-16"),
-			("Revenge of the Sith", "George Lucas", "Rick McCallum", "2005-05-19"),
-			("A New Hope", "George Lucas", "Gary Kurtz, Rick McCallum", "1977-05-25"),
-			("The Empire Strikes Back", "Irvin Kershner", "Gary Kurtz, Rick McCallum", "1980-05-17"),
-			("Return of the Jedi", "Richard Marquand", "Howard G. Kazanjian, George Lucas, Rick McCallum", "1983-05-25"),
-			("The Force Awakens", "J. J. Abrams", "Kathleen Kennedy, J. J. Abrams, Bryan Burk", "2015-12-11")
+			curs.execute("""INSERT INTO ex04_movies(title, director, producer, release_date) 
+			VALUES
+			('The Phantom Menace', 'George Lucas', 'Rick McCallum', '1999-05-19'),
+			('Attack of the Clones', 'George Lucas', 'Rick McCallum', '2002-05-16'),
+			('Revenge of the Sith', 'George Lucas', 'Rick McCallum', '2005-05-19'),
+			('A New Hope', 'George Lucas', 'Gary Kurtz, Rick McCallum', '1977-05-25'),
+			('The Empire Strikes Back', 'Irvin Kershner', 'Gary Kurtz, Rick McCallum', '1980-05-17'),
+			('Return of the Jedi', 'Richard Marquand', 'Howard G. Kazanjian, George Lucas, Rick McCallum', '1983-05-25'),
+			('The Force Awakens', 'J. J. Abrams', 'Kathleen Kennedy, J. J. Abrams, Bryan Burk', '2015-12-11')
 			""")
 			conn.commit()
 			conn.close()
@@ -42,19 +45,40 @@ def populate(request):
 			return HttpResponse(e)
 
 def display(request):
-	movielist = Movies.objects.all()
-	if len(movielist) == 0:
-		return HttpResponse("No data available")
-	return render(request, "ex04/display.html", {"movielist": movielist})
+	conn = psycopg2.connect(database="djangotraining", host="localhost", user="djangouser", password="secret")
+	with conn.cursor() as curs:
+		try:
+			movielist = []
+			curs.execute("SELECT * FROM ex04_movies")
+			r = curs.fetchall()
+			if len(r) == 0:
+				return HttpResponse("No data available")
+			for tr in r:
+				movielist.append(tr)
+			return render(request, "ex04/display.html", {"movielist": movielist})
+
+		except Exception as e:
+			return HttpResponse(e)
 
 def remove(request):
-	movielist = Movies.objects.all()
-	if len(movielist) == 0:
-		return HttpResponse("No data available")
-	if request.method == "POST":
-		form = MovieForm(request.POST)
-		if form.is_valid():
-			r = Movies.objects.filter(title=form.title)
-			r.delete()
-			redirect("/remove")
-	return render(request, "ex04/remove.html", {"movielist": movielist})
+	conn = psycopg2.connect(database="djangotraining", host="localhost", user="djangouser", password="secret")
+	with conn.cursor() as curs:
+		try:
+			if request.method == "POST":
+				form = MovieForm(request.POST)
+				if form.is_valid():
+					curs.execute("DELETE FROM ex04_movies WHERE title LIKE " + form.cleaned_data["data"])
+				else:
+					form = MovieForm()
+				return HttpResponseRedirect("/remove") # I used HttpResponseRedirect instead of simple redirect in order to modify POST method into GET method
+			movielist = []
+			curs.execute("SELECT * FROM ex02_movies")
+			r = curs.fetchall()
+			if len(r) == 0:
+				return HttpResponse("No data available")
+			for tr in r:
+				movielist.append(tr)
+			return render(request, "ex02/remove.html", {"movielist": movielist})
+		except Exception as e:
+			return HttpResponse(e)
+	
--- D05/ex00/d05/ex05/views.py
+++ D05/ex00/d05/ex05/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
+def remove(request):
+	movielist = Movies.objects.all()
+	if len(movielist) == 0:
+		return HttpResponse("No data available")
+	if request.method == "POST":
+		form = MovieForm(request.POST)
+		if form.is_valid():
+			r = Movies.objects.filter(title=form.title)
+			r.delete()
+			redirect("/remove")
+	return render(request, "ex04/remove.html", {"movielist": movielist})
