2b539e6c9ba59f91e8532fe0502f4a39862bf5ab
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-08 17:05:58 +0200

    D06: ex01 in progress

--- D06/d06/ex/forms.py
+++ D06/d06/ex/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
-class LoginForm(forms.Form):
+class RegisterForm(forms.ModelForm):
 	username = forms.CharField(required=True)
 	password = forms.CharField(required=True)
+	password_confirm = forms.CharField(required=True)
+
+	class Meta:
+		model=User
+		fields=("username", "password")
+	
+	def clean(self):
+		form_data = self.cleaned_data
+		if not "username" in form_data.keys():
+			return form_data
+		# to check if username already exist
+		if User.objects.filter(username=form_data["username"]).exists():
+			raise ValidationError("The username is already taken.")
--- D06/d06/ex/views.py
+++ D06/d06/ex/views.py
@@ -3,6 +3,7 @@ import requests
 from .forms import LoginForm
 from django.contrib import auth
 from django.conf import settings
+from django.contrib.auth.models import User
 
 def home(request):
 	namelist = settings.NAMELIST
@@ -13,3 +14,40 @@ def home(request):
 		response.set_cookie("username", cookie, max_age=settings.SESSION_COOKIE_AGE)
 		return response
 	return render(request, "main/home.html", {"namelist": namelist})
+
+def register(request):
+	if request.user.is_authenticated():
+		return redirect("/home")
+	if request.method == "POST":
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data["username"]
+			password = form.cleaned_data["password"]
+			password_confirm = form.cleaned_data["password_confirm"]
+			if password != password_confirm:
+				return HttpResponseRedirect("Password confirmation not corresponding to the password")
+			u = User.objects.create_user
+	form = LoginForm()
+	return render(request, "main/register.html", {"form": form})
+
+def login(request):
+	if request.user.is_authenticated():
+		return redirect("/home")
+	if request.method == "POST":
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data["username"]
+			password = form.cleaned_data["password"]
+			user = auth.authenticate(username=username, password=password)
+			if user and user.is_active:
+				auth.login(request, user)
+				return redirect('/home')
+			else:
+				form._error["username"] = ["This user does not exist."]
+	else:
+		form = LoginForm()
+	return render(request, 'main/home.html', {"form", form})
+
+def logout(request):
+	auth.logout(request)
+	return redirect('/home')
