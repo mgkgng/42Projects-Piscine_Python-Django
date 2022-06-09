fba212dac0308740d9dfae9f3ec3d5f1c788a594
Minguk Gaang <minguk.gaang@gmail.com>
2022-06-09 17:34:30 +0200

    D07: ex00 created

--- D07/movit/ex00/apps.py
+++ D07/movit/ex00/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MovitAppConfig(AppConfig):
+class Ex00Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movit_app'
+    name = 'ex00'
